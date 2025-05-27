5ef2b6561dbd253a913eed77c5c470bf4e1cc6bb
Juan Sanchez <juansan1993@hotmail.com>
2025-05-26 22:06:05 -0500

    Update padding input type to range

--- src/components/ChangePadding.tsx
+++ src/components/ChangePadding.tsx
@@ -17,27 +17,31 @@ export const ChangePadding = ({
       <div className="workbench__padding-wrapper">
         <div className="workbench__text-padding-y">
           <input
-            type="number"
+            type="range"
             className="workbench__input-padding-y"
             id="padding"
             placeholder={paddingY.toString()}
             min={0}
+            max={50}
+            step={1}
             value={paddingY}
             onChange={onPaddingYChange}
           />{" "}
-          <span className="workbench__padding-y">Y</span>
+          <span className="workbench__padding-y">Y&nbsp;&nbsp;{paddingY}px</span>
         </div>
         <div className="workbench__text-padding-x">
           <input
-            type="number"
+            type="range"
             className="workbench__input-padding-x"
             id="padding"
             placeholder={paddingX.toString()}
             min={0}
+            max={50}
+            step={1}
             value={paddingX}
             onChange={onPaddingXChange}
           />
-          <span className="workbench__padding-x">X</span>
+          <span className="workbench__padding-x">X&nbsp;&nbsp;{paddingX}px</span>
         </div>
       </div>
     </>
