af9f0c682d2b908a0736fb947ebc301a249fdf37
Juan Sanchez <juansan1993@hotmail.com>
2025-09-11 13:15:48 -0500

    Remove test layout

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,16 +1,16 @@
 export const Test = () => {
   return (
     <div className="test">
-      <div className="test__column_one column">test</div>
-      <div className="test__column_two column">
-        <div className="test__row_one row">
+      <div className="test__column--one column">test</div>
+      <div className="test__column--two column">
+        <div className="test__row--one row">
           <p>test</p>
         </div>
-        <div className="test__row_two row">
+        <div className="test__row--two row">
           <p>test</p>
         </div>
       </div>
-      <div className="test__column_three column"></div>
+      <div className="test__column--three column"></div>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
-// import App from "./App.tsx";
-import { Test } from "./components/Test.tsx";
+import App from "./App.tsx";
+// import { Test } from "./components/Test.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      {/* <App /> */}
-      <Test />
+      <App />
+      {/* <Test /> */}
     </ThemeProvider>
   </StrictMode>
 );
