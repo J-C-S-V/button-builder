96924ca9e424d3020a3fe735fedc7b06b07997b1
Juan Sanchez <juansan1993@hotmail.com>
2025-09-22 21:54:55 -0500

    Update middle section styles

--- src/components/Languages/Languages.tsx
+++ src/components/Languages/Languages.tsx
@@ -3,8 +3,8 @@ import styles from "./Languages.module.css";
 export const Languages = () => {
   return (
     <div className={styles.languages}>
-      <button className="css">CSS</button>
-      <button className="html">HTML</button>
+      <button className={`${styles.languages__css} ${styles.languages__button}`}>CSS</button>
+      <button className={`${styles.languages__html} ${styles.languages__button}`}>HTML</button>
     </div>
   );
 };
