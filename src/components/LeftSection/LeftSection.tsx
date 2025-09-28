76a8f437c7318f2912e229d1efc9077f8d1a437d
Juan Sanchez <juansan1993@hotmail.com>
2025-09-28 15:44:13 -0500

    Add more buttons to the left section

--- src/components/LeftSection/LeftSection.tsx
+++ src/components/LeftSection/LeftSection.tsx
@@ -4,9 +4,9 @@ export const LeftSection = () => {
   return (
     <div className={styles["left-section"]}>
       <button className={styles["left-section__button"]}>Click me!</button>
-      <button className={styles["left-section__button"]}>Click me!</button>
-      <button className={styles["left-section__button"]}>Click me!</button>
-      <button className={styles["left-section__button"]}>Click me!</button>
+      <button className={styles["left-section__button--two"]}>Click me!</button>
+      <button className={styles["left-section__button--three"]}>Click me!</button>
+      <button className={styles["left-section__button--four"]}>Click me!</button>
     </div>
   );
 };
