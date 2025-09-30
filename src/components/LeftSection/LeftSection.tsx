cde8aa8a73f05ce3dc47c38be9edfbba6110ba98
Juan Sanchez <juansan1993@hotmail.com>
2025-09-29 23:09:31 -0500

    Add button six

--- src/components/LeftSection/LeftSection.tsx
+++ src/components/LeftSection/LeftSection.tsx
@@ -8,11 +8,11 @@ export const LeftSection = () => {
       <button className={styles["left-section__button--three"]}>Click me!</button>
       <button className={styles["left-section__button--four"]}>Click me!</button>
       <button className={styles["left-section__button--five"]}>Click me!</button>
-      <button className={styles["left-section__button--four"]}>Click me!</button>
-      <button className={styles["left-section__button--four"]}>Click me!</button>
-      <button className={styles["left-section__button--four"]}>Click me!</button>
-      <button className={styles["left-section__button--four"]}>Click me!</button>
-      <button className={styles["left-section__button--four"]}>Click me!</button>
+      <button className={styles["left-section__button--six"]}>Click me!</button>
+      <button className={styles["left-section__button--seven"]}>Click me!</button>
+      <button className={styles["left-section__button--eight"]}>Click me!</button>
+      <button className={styles["left-section__button--nine"]}>Click me!</button>
+      <button className={styles["left-section__button--ten"]}>Click me!</button>
     </div>
   );
 };
