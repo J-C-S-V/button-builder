f1756d0688474efd1ab06cdad559e9ee8181e120
Juan Sanchez <juansan1993@hotmail.com>
2025-10-01 21:47:40 -0500

    Remove useRef from languages component

--- src/components/MidSection/Languages/Languages.tsx
+++ src/components/MidSection/Languages/Languages.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import styles from "./Languages.module.css";
 
 export const Languages = ({
@@ -34,44 +34,24 @@ export const Languages = ({
   boxShadowColor: string;
   boxShadowSpread: number;
 }) => {
-  const textareaRefHTML = useRef<HTMLTextAreaElement>(null);
-  const textareaRefCSS = useRef<HTMLTextAreaElement>(null);
   const [copied, setCopied] = useState(false);
 
   const handleCopyHTML = () => {
-    if (textareaRefHTML.current) {
-      navigator.clipboard
-        .writeText(textareaRefHTML.current.value)
-        .then(() => {
-          setCopied(true);
-          setTimeout(() => setCopied(false), 1000);
-        })
-        .catch((err) => {
-          console.error("Failed to copy:", err);
-        });
-    }
+    navigator.clipboard
+      .writeText(`<a class="button">${innerText}</a>`)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
   };
 
   const handleCopyCSS = () => {
-    if (textareaRefCSS.current) {
-      navigator.clipboard
-        .writeText(textareaRefCSS.current.value)
-        .then(() => {
-          setCopied(true);
-          setTimeout(() => setCopied(false), 1000);
-        })
-        .catch((err) => {
-          console.error("Failed to copy:", err);
-        });
-    }
-  };
-  return (
-    <div className={styles.languages}>
-      <textarea className={styles.languages__textarea} ref={textareaRefHTML} value={`<a class="button">${innerText}</a>`} />
-      <textarea
-        className={styles.languages__textarea}
-        ref={textareaRefCSS}
-        value={`.button {
+    navigator.clipboard
+      .writeText(
+        `.button {
   cursor: pointer;
   background-color: ${backgroundColor};
   border-radius: ${borderRadius}px;
@@ -80,9 +60,18 @@ export const Languages = ({
   font-size: ${fontSize}px;
   color: ${fontColor};
   border: ${border}px solid ${borderColor};
-  box-shadow: ${boxShadowX}px ${boxShadowY}px ${boxShadowBlur}px ${boxShadowSpread}px ${boxShadowColor};
-}`}
-      />
+  box-shadow: ${boxShadowX}px ${boxShadowY}px ${boxShadowBlur}px ${boxShadowSpread}px ${boxShadowColor};}`
+      )
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
+  };
+  return (
+    <div className={styles.languages}>
       {copied && <div className={styles.languages__copied}>Copied!</div>}
       <button onClick={handleCopyCSS} className={`${styles.languages__css} ${styles.languages__button}`}>
         Get CSS
