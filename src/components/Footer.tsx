ac7edc9cceb1a61ddbfe1129fd355b4c0c1e7caf
Juan Sanchez <juansan1993@hotmail.com>
2025-10-02 21:32:06 -0500

    Update footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,15 @@
 import { IconGithub } from "../assets/Icons";
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
-    <footer style={{ position: "absolute", bottom: "0", color: "var(--color-text-white)", fontSize: "1rem" }} className="footer">
-      <p className="footer__text">
-        © 2025 Free Button Builder. All rights reserved. Made with ❤ by Juan Sanchez.
-        <a target="_blank" href="https://github.com/J-C-S-V">
-          <span className="footer__tooltip">
-            <IconGithub />
-          </span>
-        </a>
-      </p>
+    <footer className={styles["footer"]}>
+      <p className={styles["footer__text"]}>© 2025 Free Button Builder. All rights reserved. Made with ❤ by Juan Sanchez.</p>
+      <a className={styles["footer__anchor"]} target="_blank" href="https://github.com/J-C-S-V">
+        <span className={styles["footer__tooltip"]}>
+          <IconGithub />
+        </span>
+      </a>
     </footer>
   );
 };
