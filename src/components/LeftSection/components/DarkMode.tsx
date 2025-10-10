7ce0529147217c51360ffeb87db79772fc03c8a0
Juan Sanchez <juansan1993@hotmail.com>
2025-10-09 22:39:46 -0500

    Update darkmode button style

--- src/components/LeftSection/components/DarkMode.tsx
+++ src/components/LeftSection/components/DarkMode.tsx
@@ -4,19 +4,15 @@ import { SunIcon, MoonIcon } from "../../../../public/assets/svg/Icons";
 import styles from "./DarkMode.module.css";
 
 export const DarkMode = () => {
-  const { handleClickTheme, theme } = useContext(ThemeContext);
+  const { handleClickTheme, theme, right } = useContext(ThemeContext);
 
   return (
     <div className={styles["dark-mode"]} onClick={handleClickTheme}>
-      {/* {theme === "dark" ? <SunIcon /> : <MoonIcon />} */}
-      <div className={styles["dark-mode"]}>
-        <span className={styles["dark-mode__sun"]}>
-          <SunIcon />
-        </span>
-        <span className={styles["dark-mode__moon"]}>
-          <MoonIcon />
-        </span>
+      <SunIcon />
+      <div className={styles["dark-mode__icon-bg"]}>
+        <div className={styles["dark-mode__icon-circle"]}></div>
       </div>
+      <MoonIcon />
     </div>
   );
 };
