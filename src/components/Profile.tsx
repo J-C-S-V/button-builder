7880e38de1291fe8332398b911a544f4e598fd3e
Juan Sanchez <juansan1993@hotmail.com>
2025-10-20 09:32:38 -0500

    Add form template

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { auth } from "../lib/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import styles from './Profile.module.css';
+
 
 export const Profile = () => {
   const [user, setUser] = useState<string | null>(null);
@@ -17,41 +19,50 @@ export const Profile = () => {
       });
   };
 
-  const handleClickSignIn = () => {
-    signInWithEmailAndPassword(auth, "jota@test.com", "test123**//lksjdkfj")
-      .then((response) => {
-        const user = response.user;
-        console.log(user);
-        setUser(user.email);
-      })
-      .catch((error) => {
-        console.error("Error signing in: ", error);
-      });
-  };
+  // const handleClickSignIn = () => {
+  //   signInWithEmailAndPassword(auth, "jota@test.com", "test123**//lksjdkfj")
+  //     .then((response) => {
+  //       const user = response.user;
+  //       console.log(user);
+  //       setUser(user.email);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error signing in: ", error);
+  //     });
+  // };
 
-  const handleClickSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        setUser(null);
-      })
-      .catch((error) => {
-        console.error("Error signing out: ", error);
-      });
-  };
+  // const handleClickSignOut = () => {
+  //   signOut(auth)
+  //     .then(() => {
+  //       setUser(null);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error signing out: ", error);
+  //     });
+  // };
+
   return (
-    <div className="profile-pending">
-      {user && (
-        <>
-          <p>Hello {user}</p>
-          <button onClick={handleClickSignOut}>Sign Out</button>
-        </>
-      )}
-      {!user && (
-        <>
-          <button onClick={handleClickSignIn}>Sign In</button>
-          <button onClick={handleClickSignUp}>Sign Up</button>
-        </>
-      )}
-    </div>
+    // <div className="profile-pending">
+    //   {user && (
+    //     <>
+    //       <p>Hello {user}</p>
+    //       <button onClick={handleClickSignOut}>Sign Out</button>
+    //     </>
+    //   )}
+    //   {!user && (
+    //     <>
+    //       <button onClick={handleClickSignIn}>Sign In</button>
+    //     </>
+    //   )}
+    // </div>
+    <>
+      <form className={styles['form']}>
+        <label htmlFor="email" className={styles['form__label']}>Email: </label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password" className={styles['form__label']}>Password: </label>
+        <input type="password" name="" />
+        <button className={styles['form__signup']} onClick={handleClickSignUp}>Sign Up</button>
+      </form>
+    </>
   );
 };
