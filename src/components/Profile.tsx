709a525a70d6d88c594946f3d64746fe2679392a
Juan Sanchez <juansan1993@hotmail.com>
2025-10-20 22:17:47 -0500

    Update form logic

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { auth } from "../lib/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useForm } from "react-hook-form";
+import { FormData } from '../types/types'
 import styles from './Profile.module.css';
 
 
 export const Profile = () => {
   const [user, setUser] = useState<string | null>(null);
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<FormData>();
+  // const newItem = register('registerName');
+  // console.log(newItem)
+  const onSubmit = handleSubmit((data) => {
+        console.log(data)
+      })
 
   const handleClickSignUp = () => {
     createUserWithEmailAndPassword(auth, "new@test.com", "test12*//lksjdkfj")
@@ -56,12 +64,67 @@ export const Profile = () => {
     //   )}
     // </div>
     <>
-      <form className={styles['form']}>
+      <form onSubmit={onSubmit} className={styles['form']}>
         <label htmlFor="email" className={styles['form__label']}>Email: </label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          {...register('email', { 
+            required: {
+              value: true,
+              message: 'Email is required'
+            },
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+              message: 'Invalid email format'
+            }
+          }
+        )
+      }/>
+        {errors.email && <span className={styles['error']}>{ errors.email.message }</span>}
         <label htmlFor="password" className={styles['form__label']}>Password: </label>
-        <input type="password" name="password" id="password" />
+        <input 
+          type="password"
+          {...register('password', { 
+            required: {
+              value: true,
+              message: 'Password is required'
+            },
+          minLength: {
+            value: 10,
+            message: 'Minimum length is 10 characters'
+          },
+          validate: {
+            hasNumber: (value) => /[0-9]/.test(value) || 'Password must contain at least one number',
+            // hasSpecialChar: (value) => /[!@#$%^&*(),.?":{}|<>]/.test(value) || 'Password must contain at least one special character',
+            // hasUpperCase: (value) => /[A-Z]/.test(value) || 'Password must contain at least one uppercase letter'
+          }
+        })}/>
+        {errors.password && <span className={styles['error']}>{errors.password.message}</span>}
+        <label htmlFor="confirmPassword" className={styles['form__label']}>Confirm password: </label>
+        <input 
+          type="password"
+          {...register('confirmPassword', {
+            required: {
+              value: true,
+              message: 'Password is required'
+            },
+          minLength: {
+            value: 10,
+            message: 'Minimum length is 10 characters'
+          },
+          validate: (value) => {
+            if(value === watch('password')) {
+              return true;
+            } else {
+              return 'Passwords do not match';
+            }
+          }
+        })}/>
+        {errors.confirmPassword && <span className={styles['error']}>{errors.confirmPassword.message}</span>}
         <button className={styles['form__signup']} onClick={handleClickSignUp}>Sign Up</button>
+        <pre>
+          {JSON.stringify(watch(), null, 2)}
+        </pre>
       </form>
     </>
   );
