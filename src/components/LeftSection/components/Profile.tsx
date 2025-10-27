17914a4f715da7e40fbc92ac979f338c66a0c0fe
Juan Sanchez <juansan1993@hotmail.com>
2025-10-26 22:04:31 -0500

    Style sign in and sign up buttons

--- src/components/LeftSection/components/Profile.tsx
+++ src/components/LeftSection/components/Profile.tsx
@@ -3,8 +3,8 @@ import styles from "./Profile.module.css";
 export const Profile = () => {
   return (
     <div className={styles["profile"]}>
-      <button className={styles["profile__signup"]}>Sign Up</button>
-      <button className={styles["profile__signup"]}>Sign In</button>
+      <button className={styles["profile__button"]}>Sign Up</button>
+      <button className={styles["profile__button"]}>Sign In</button>
     </div>
   );
 };
