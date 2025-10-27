f6e055b2486d06867cfbdeac4d8ef5f62e35ee8e
Juan Sanchez <juansan1993@hotmail.com>
2025-10-26 22:10:58 -0500

    Update file names

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext";
-import "./index.css";
+import "./globals.css";
 import App from "./App.tsx";
 // import { Test } from "./components/Test.tsx";
 import "./App.module.css";
