d4ceb333eb6a6bcfb8177c683c5394adf31fe94f
Juan Sanchez <juansan1993@hotmail.com>
2025-11-07 21:13:37 -0500

    Add cursor component

---
+++ src/components/RightSection/Cursor/Components/FontSize.tsx
@@ -0,0 +1,32 @@
+import styles from "./FontSize.module.css";
+
+export const FontSize = ({
+  fontSize,
+  onFontSizeChange,
+}: {
+  fontSize: number;
+  onFontSizeChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  return (
+    <div className={styles["font-size"]}>
+      <label htmlFor="fontSize" className={styles["font-size__label"]}>
+        Font Size:
+      </label>
+      <div className={styles["font-size__input-container"]}>
+        <input
+          className={styles["font-size__input"]}
+          type="range"
+          id="fontSize"
+          min={0}
+          max={100}
+          step={1}
+          value={fontSize}
+          onChange={onFontSizeChange}
+        />
+        <div className={styles["font-size__span-container"]}>
+          <span className={styles["font-size__span"]}>{fontSize}px</span>
+        </div>
+      </div>
+    </div>
+  );
+};
